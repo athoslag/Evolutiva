6a839056a460c850bc712def81b9d30d9b4ae7e1
Pedro Costa <pedronerung@gmail.com>
2018-10-19 22:16:40 -0300

    Store individuals already created to be able to compare different selectors

--- src/util/IndividualsGenerator.py
+++ src/util/IndividualsGenerator.py
@@ -12,15 +12,18 @@ class IndividualsGenerator(object):
 
     def __init__(self, total_genes):
         self.total_genes = total_genes
+        self.individuals = []
         pass
 
     def generate_random_individuals(self, popsize):
-        individuals = []
+
+        if self.individuals:
+            return self.individuals
 
         for _ in range(popsize):
             dna = np.random.randint(2, size=self.total_genes)
             genotype = Genotype(dna=dna, m_rate=0.5)
             individual = Individual(genotype)
-            individuals.append(individual)
+            self.individuals.append(individual)
 
-        return individuals
+        return self.individuals
